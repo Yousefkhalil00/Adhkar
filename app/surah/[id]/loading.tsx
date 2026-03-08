749dcf0fd1edc5b2f7d04be2a5a8b61547601d8c
yousefkhalil894@gmail.com <yousefkhalil894@gmail.com>
2026-03-08 22:06:00 +0200

    modified:   app/surah/[id]/loading.tsx

--- app/surah/[id]/loading.tsx
+++ app/surah/[id]/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <p className="text-xl text-white"> تحميل السورة</p>
+      <p className="text-xl text-white"> تحميل السورة...</p>
     </div>
   );
 }
